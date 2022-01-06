baa67769e1e1e145ce9aff76e625688236009b19
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-01-06 21:31:17 +0100

    Сделал дз №8

---
+++ src/tutorial-8/pages/About.jsx
@@ -0,0 +1,9 @@
+import { Card } from "react-bootstrap";
+
+export default function About() {
+    return (
+        <Card>
+          <Card.Body>Это мой личный сайт!</Card.Body>
+        </Card>
+    );
+}
---
+++ src/tutorial-8/pages/Article.jsx
@@ -0,0 +1,13 @@
+export default function Article({ id }) {
+    return (
+        <div>
+            <h1>Статья №{id}</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae distinctio fuga animi
+                aliquam sit id veritatis, doloribus ducimus quas, dignissimos non minima quia amet possimus?
+                Impedit nemo ducimus fuga rem!
+            </p>
+            <a href="/"><button>Назад</button></a>
+        </div>
+    );
+}
