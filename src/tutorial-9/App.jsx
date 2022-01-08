e66a589f89aa602f26f23130c64ca2af6692eac8
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-01-08 18:43:34 +0100

    поправил ошибку

--- src/tutorial-9/App.jsx
+++ src/tutorial-9/App.jsx
@@ -3,19 +3,19 @@ import React from "react";
 export default function App() {
     const [users, setUsers] = React.useState([]);
 
-    const getUsers = async () => {
-        let response = await fetch('https://61d8e2cfe6744d0017ba8cdc.mockapi.io/users');
-        let data = await response.json();
-        return data;
-    }
-
-    async function updateUsers() {
+    const updateUsers = async () => {
         try {
-            const usersData = await getUsers();
-            setUsers(usersData);
-        }
-        catch(e) {
-            console.log(e);
+            let response = await fetch('https://61d8e2cfe6744d0017ba8cdc.mockapi.io/users');
+
+            if (!response.ok) {
+                throw Error('Произошла ошибка!');  
+            }
+
+            let data = await response.json();
+
+            setUsers(data);
+        } catch(e) {
+            console.log(e?.message);
         }
     }
 
